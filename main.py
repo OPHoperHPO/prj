12264931cee407d021888c2c6d834d4a5cbaafc4
Максим Костюков <enginer385@gmail.com>
2021-09-25 02:39:08 +1100

    Added db session generator

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
+from fastapi import Depends
 from fastapi.responses import JSONResponse
-import db
+from db import SessionLocal
 
 app = FastAPI()
 
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 @app.get("/hello_world")
-def hello_world():
+def hello_world(db=Depends(get_db)):
     return JSONResponse(status_code=200, content={
         "hello": "world",
     })
